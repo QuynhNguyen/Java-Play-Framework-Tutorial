d8a24f71f7e1a2d52bd3ca6b0d8d986fa211ed6d
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-12-10 23:34:53 -0800

    added content to front page and various layout designs

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,23 @@
 package controllers;
 
-import play.*;
+import java.util.List;
+
+import models.Post;
+import play.Play;
 import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
 
 public class Application extends Controller {
 
+	@Before
+	public static void addDefault(){
+		renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
+	
     public static void index() {
-		System.out.print("YOP");
-        render();
+		Post frontPost = Post.find("order by postedAt desc").first();
+		List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+		render(frontPost, olderPosts);
     }
 
 }
